bb9b022820e09e322ac0f4cf306c74a7f0b7dea2
Ebuka <sebuka559@gmail.com>
2025-04-11 09:43:35 +0100

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['blog-l12q.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['13.48.59.113', 'localhost', '127.0.0.1']
 
 
 # Application definition
