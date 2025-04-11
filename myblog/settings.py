69e27f627c5c528fa6ff01ae5e3e2bdd2948015a
Ebuka <sebuka559@gmail.com>
2025-04-11 12:37:55 +0100

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['13.48.59.113', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['56.228.26.208', 'localhost', '127.0.0.1']
 
 
 # Application definition
